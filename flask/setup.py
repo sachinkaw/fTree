083d379d44eb2c000724b97282398a958521f2d6
Sachin Kaw <sachinkaw@gmail.com>
2019-01-25 14:26:40 +1300

    Updated Files

--- flask/setup.py
+++ flask/setup.py
@@ -4,8 +4,6 @@ setup(
 	name='JSONRequests',
 	version='1.0',
 	description='d3js Tree Editor server',
-	author='Adam Feuer', author_email='adam@adamfeuer.com',
-	url='http://adamfeuer.com/',
 	install_requires=['flask', 'requests']
 )
 
