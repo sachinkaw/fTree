949aafd3147b4e89a35b7f229d0e93bc707c2d7e
Rich Churcher <rich.churcher@gmail.com>
2019-02-04 13:14:03 +1300

    Save/load

--- js/index.js
+++ js/index.js
@@ -1,133 +1,70 @@
-var whakapapa = new Tree({
-  extra: {
-    id: "292203a6-7ba5-43d6-9eb0-e526a2cbe282"
-  },
-  name: "Manawa",
-  children: [],
-  marriages: [
-    {
-      spouse: {
-        extra: {
-          id: "6a29f0c3-5d03-458a-8bff-7e478c7bbdf1"
-        },
-        name: "Kahu",
-        children: [],
-        marriages: []
-      },
-      children: [
-        {
-          extra: {
-            id: "f95cfd2d-23bc-4f71-8b91-007ee39c4a93"
-          },
-          name: "Rawiri",
-          children: [],
-          marriages: [
-            {
-              spouse: {
-                extra: {
-                  id: "7ac3269c-b37c-43d4-80bc-81bab843d515"
-                },
-                name: "Awhina",
-                children: [],
-                marriages: []
-              },
-              children: [
-                {
-                  extra: {
-                    id: "61b5a735-b396-4aac-909b-273551d3395e"
-                  },
-                  name: "Tui",
-                  children: [],
-                  marriages: []
-                }
-              ]
-            },
-            {
-              spouse: {
-                extra: {
-                  id: "5abacf1b-b194-4904-9b62-f11c2cb0119d"
-                },
-                name: "Kaia",
-                children: [],
-                marriages: []
-              },
-              children: [
-                {
-                  extra: {
-                    id: "dab85dc6-8bcc-4ec1-93b8-edad8917dbb7"
-                  },
-                  name: "Manaia",
-                  children: [],
-                  marriages: []
-                }
-              ]
-            }
-          ]
-        },
-        {
-          extra: {
-            id: "e86dd806-d3ba-4c50-a52e-8a076359a750"
-          },
-          name: "Te Aroha",
-          children: [],
-          marriages: []
-        }
-      ]
-    }
-  ]
-});
+var whakapapa = null;
 
-// var menu = [
-//   {
-//     title: "Rename node",
-//     action: function(elm, d, i) {
-//       console.log("Rename node");
-//       $("#RenameNodeName").val(d.name);
-//       rename_node_modal_active = true;
-//       node_to_rename = d;
-//       $("#RenameNodeName").focus();
-//       $("#RenameNodeModal").foundation("reveal", "open");
-//     }
-//   },
-//   {
-//     title: "Delete node",
-//     action: function(elm, d, i) {
-//       console.log("Delete node");
-//       delete_node(d);
-//     }
-//   },
-//   {
-//     title: "Create child node",
-//     action: function(elm, d, i) {
-//       console.log("Create child node");
-//       create_node_parent = d;
-//       create_node_modal_active = true;
-//       $("#CreateNodeModal").foundation("reveal", "open");
-//       $("#CreateNodeName").focus();
-//     }
-//   },
-//   {
-//     title: "Create sibling node",
-//     action: function(elm, d, i) {
-//       console.log("Create sibling node");
-//       create_node_parent = d;
-//       create_node_modal_active = true;
-//       $("#CreateNodeModalSibling").foundation("reveal", "open");
-//       $("#CreateNodeNameSibling").focus();
-//     }
-//   },
-//   {
-//     title: "Create parent node",
-//     action: function(elm, d, i) {
-//       console.log("Create parent node");
-//       create_node_parent = d;
-//       create_node_modal_active = true;
-//       $("#CreateNodeModalParent").foundation("reveal", "open");
-//       $("#CreateNodeNameParent").focus();
-//     }
-//   }
-// ];
-//
+function loadTree() {
+  $.get("/tree", function(data) {
+    whakapapa = new Tree(JSON.parse(data));
+    updateTree();
+  }).fail(function() {
+    toastr.error("Couldn't load tree.");
+  });
+}
+
+loadTree();
+
+function updateTree() {
+  // TODO: Ideally, would be nice not to recreate the whole graph on changes.
+  $("#tree-container").empty();
+  dTree.init([whakapapa.tree], {
+    target: "#tree-container",
+    callbacks: {
+      nodeRightClick: contextMenu
+    },
+    height: 800,
+    width: 800
+  });
+}
+
+function contextMenu(name, extra, id) {
+  var $target = $(d3.event.target);
+  var contextMenu = new Foundation.ContextMenu($target, {
+    position: d3.event,
+    structure: [
+      {
+        text: "Rename",
+        click: function() {
+          $("#RenameNodeName").val(name);
+          $("#RenameNodeModal").data("nodeId", extra.id);
+          openModal("#RenameNodeModal");
+        }
+      },
+      {
+        text: "Create parent",
+        click: function() {
+          $("#CreateNodeModalParent").data("nodeId", extra.id);
+          openModal("#CreateNodeModalParent");
+        }
+      },
+      {
+        text: "Create child",
+        click: function() {
+          $("#CreateNodeModal").data("nodeId", extra.id);
+          openModal("#CreateNodeModal");
+        }
+      },
+      {
+        text: "Create sibling",
+        click: function() {
+          $("#CreateNodeModalSibling").data("nodeId", extra.id);
+          openModal("#CreateNodeModalSibling");
+        }
+      }
+    ]
+  });
+
+  $target.one("hide.zf.contextmenu", function() {
+    contextMenu.destroy();
+  });
+}
 
 function renameNode() {
   try {
@@ -184,16 +121,18 @@ function createParent() {
   }
 }
 
-function updateTree() {
-  // TODO: Ideally, would be nice not to recreate the whole graph on changes.
-  $("#tree-container").empty();
-  dTree.init([whakapapa.tree], {
-    target: "#tree-container",
-    callbacks: {
-      nodeRightClick: contextMenu
+function saveTree() {
+  $.ajax({
+    url: "/tree",
+    type: "POST",
+    data: JSON.stringify(whakapapa.tree),
+    contentType: "application/json; charset=utf-8",
+    success: function() {
+      toastr.success("Tree saved.");
     },
-    height: 800,
-    width: 800
+    fail: function() {
+      toastr.error("Couldn't save tree.");
+    }
   });
 }
 
@@ -205,47 +144,3 @@ function openModal(id) {
 function closeModal(id) {
   $(id).foundation("close");
 }
-
-function contextMenu(name, extra, id) {
-  var $target = $(d3.event.target);
-  var contextMenu = new Foundation.ContextMenu($target, {
-    position: d3.event,
-    structure: [
-      {
-        text: "Rename",
-        click: function() {
-          $("#RenameNodeName").val(name);
-          $("#RenameNodeModal").data("nodeId", extra.id);
-          openModal("#RenameNodeModal");
-        }
-      },
-      {
-        text: "Create parent",
-        click: function() {
-          $("#CreateNodeModalParent").data("nodeId", extra.id);
-          openModal("#CreateNodeModalParent");
-        }
-      },
-      {
-        text: "Create child",
-        click: function() {
-          $("#CreateNodeModal").data("nodeId", extra.id);
-          openModal("#CreateNodeModal");
-        }
-      },
-      {
-        text: "Create sibling",
-        click: function() {
-          $("#CreateNodeModalSibling").data("nodeId", extra.id);
-          openModal("#CreateNodeModalSibling");
-        }
-      }
-    ]
-  });
-
-  $target.one("hide.zf.contextmenu", function() {
-    contextMenu.destroy();
-  });
-}
-
-updateTree();
